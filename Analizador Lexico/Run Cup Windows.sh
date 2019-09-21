f18ae8e50e13c43bc99e998378b33869890e1b62
Alexy Cruz <alexyc@unitec.edu>
2019-09-21 15:57:12 -0600

    run cup for ubuntu and windows fixed

--- Analizador Lexico/Run Cup Windows.sh
+++ Analizador Lexico/Run Cup Windows.sh
@@ -7,5 +7,9 @@ java -cp java -classpath "./java-cup-11b.jar";. java_cup.Main < parser.cup
 echo -----------------------Running Main.java--------------------------------------------
 javac -classpath "./java-cup-11b.jar";. Main.java *.java
 echo -----------------------Running Example codes----------------------------------------
-java -classpath "./java-cup-11b.jar";. Main ./Codigos\ de\ ejemplo/intercambio.txt
+read -p "Enter File Name: " fileName
+read -p "Enter Extension: " fileExtension
+fullFile="$fileName.$fileExtension"
+echo ------------------------Running Example Codes---------------------------------------
+java -classpath "./java-cup-11b.jar";. Main ./Codigos\ de\ ejemplo/"$fullFile"
 echo -------------------------------END--------------------------------------------------
--- Analizador Lexico/Run Cup.sh
+++ Analizador Lexico/Run Cup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 clear
-echo ------------------------Running Lexer=----------------------------------------------
+echo ------------------------Running Lexer-----------------------------------------------
 jflex AnalizadorLexico.flex
 echo ------------------------Running parser.cup------------------------------------------
 java -cp .:java-cup-11b.jar java_cup.Main < parser.cup
 echo ------------------------Running Main.java-------------------------------------------
 javac -cp .:java-cup-11b.jar Main.java
 echo ------------------------Running Example Codes---------------------------------------
-java -cp .:java-cup-11b.jar Main ./Codigos\ de\ ejemplo/entrada1.txt
+read -p "Enter File Name: " fileName
+read -p "Enter Extension: " fileExtension
+fullFile="$fileName.$fileExtension"
+echo ------------------------Running Example Codes---------------------------------------
+java -cp .:java-cup-11b.jar Main ./Codigos\ de\ ejemplo/"$fullFile"
 echo --------------------------------END-------------------------------------------------
