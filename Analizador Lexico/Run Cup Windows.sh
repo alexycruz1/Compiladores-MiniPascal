9bbbe071d34304c39b40d1137b7300d2038a4feb
Alexy Cruz <alexyc@unitec.edu>
2019-09-21 16:02:29 -0600

    Run cup in ubuntu and in windows fixed

--- Analizador Lexico/Run Cup Windows.sh
+++ Analizador Lexico/Run Cup Windows.sh
@@ -7,7 +7,9 @@ java -cp java -classpath "./java-cup-11b.jar";. java_cup.Main < parser.cup
 echo -----------------------Running Main.java--------------------------------------------
 javac -classpath "./java-cup-11b.jar";. Main.java *.java
 echo -----------------------Running Example codes----------------------------------------
+echo "Enter File Name (Without File Extension)"
 read -p "Enter File Name: " fileName
+echo "Enter File Extension (e.g: txt, csv, class, java)"
 read -p "Enter Extension: " fileExtension
 fullFile="$fileName.$fileExtension"
 echo ------------------------Running Example Codes---------------------------------------
