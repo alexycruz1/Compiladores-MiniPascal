a1f3c0bf00a255f5866c230e47108ae386b6505e
CJAlvarez <cristhianalvarez96@gmail.com>
2019-11-26 07:41:26 -0600

    Before merge

--- Analizador Lexico/Run Lexer.sh
+++ Analizador Lexico/Run Lexer.sh
@@ -1,5 +1,22 @@
 #!/bin/bash  
 clear
+
+if [ -f ./AnalizadorLexico.java ]; then
+  
+  set -e
+  echo Remove AnalizadorLexico.java
+  echo
+  rm AnalizadorLexico.java
+
+fi
+
+if [ -f ./AnalizadorLexico.java~ ]; then
+
+  echo Remove AnalizadorLexico.java~
+  echo
+  rm AnalizadorLexico.java~
+fi
+
 echo ------------------------------------------------------------------------------------
 jflex AnalizadorLexico.flex
 echo ------------------------------------------------------------------------------------
